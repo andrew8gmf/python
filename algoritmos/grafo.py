8d212310e96b2b74ac1f9145501bd36622b25892
andrew8gmf <andrew8gmf@gmail.com>
2020-09-03 15:59:49 -0300

    update exercise

--- algoritmos/grafo.py
+++ algoritmos/grafo.py
@@ -17,6 +17,14 @@ def imprime_grafo(g):
 def grau(a):
     return len(a)
 
+def adjacentes(g, b):
+    return g[x]
+
+def arestas(g, b, c):
+    if c in g[x]:
+        return "Existe aresta entre eles!"
+
+# Programa principal
 grafo = { }
 while True:
     x = input("Entre com o vértice ou FIM para encerrar: ")
@@ -28,8 +36,24 @@ while True:
 
 imprime_grafo(grafo)
 
+# Visualizar grau de um vertice
 x = input("Digite o vertice que deseja saber o grau:")
 if x in grafo:
     print("Grau do vertice", x, " = ", grau(grafo[x]))
 else:
-    print("Não existe vértice")
+    print("Não existe vértice!")
+
+# Visualizar os vertices adjacentes de um determinado vertice
+x = input("Digite o vertice que deseja saber seus adjacentes:")
+if x in grafo:
+    print("Adjacentes do vertice:", x, " = ", adjacentes(grafo, x))
+else:
+    print("Não existe esse vertice!")
+
+# Verificar se existe aresta entre os vertices
+x = input("Digite o primeiro vertice:")
+y = input("Digite o segundo vertice:")
+if x in grafo and y in grafo:
+    print(arestas(grafo, x, y))
+else:
+    print("Um dos vertices não existe!")
