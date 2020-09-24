9eee2a3e62b06a34c2c2c6fb1b71ff9b22e9d8d8
andrew8gmf <andrew8gmf@gmail.com>
2020-09-24 17:56:11 -0300

    create bfs

---
+++ algoritmos/buscaGrafo.py
@@ -0,0 +1,27 @@
+def insere_aresta(g, a, b):
+    if a not in g:
+        g[a]=[]
+    if b not in g:
+        g[b]=[]
+    g[a].append(b)
+    g[b].append(a)
+
+def imprime_grafo(g):
+    for v,a in g.items():
+        print("="*50)
+        print('Vertice:', v)
+        print('Adjacentes:', a)
+
+def adjacentes(g, v):
+    return g[v]
+
+grafo = { }
+while True:
+    x = input("Entre com o vértice ou FIM para encerrar: ")
+    if x=='FIM':
+        break
+    y = input("Entre com um vértice adjacente:")
+
+    insere_aresta(grafo, x, y)
+
+imprime_grafo(grafo)
