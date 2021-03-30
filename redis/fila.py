788e6b36385eb89b57891ff9fb756886a1ea599a
andrew8gmf <andrew8gmf@gmail.com>
2021-03-30 15:45:19 -0300

    Update: Redis fila.py Brunão version

--- redis/fila.py
+++ redis/fila.py
@@ -1,25 +1,41 @@
 import redis
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+def UltimoFila():
+    if(r.lrange("fila", 0, 0)==[]):
+        return 1000
+    else:
+        t = r.lrange("fila", -1, -1)
+        return int(t[0]) + 1
 
 def Adicionar():
-    nome = str(input("\nNome: "))
-    email = str(input("Email: "))
-    telefone = str(input("Telefone: "))
-    r.rpush('fila', email)
+    nome = str(input("\nnome:"))
+    email = str(input("e-mail:"))
+    telefone = str(input("telefone:"))
+    key = UltimoFila()
+    r.hset(key, "id", key)
+    r.hset(key, "nome", nome)
+    r.hset(key, "email", email)
+    r.hset(key, "telefone", telefone)
+    r.rpush("fila", key)
 
 def Exibir():
-    print("\nFila: ")
-    print(r.lrange('fila', 0, -1))
+    user = r.lrange("fila", 0, -1)
+    for u in user:
+        print(r.hgetall(u))
 
 def Remover():
-    index = r.lindex('fila', 0)
-    print("\nRemovido: ")
-    print(index)
-    r.ltrim('fila', 1, -1)
+    if(r.lrange("fila", 0, 0)==[]):
+        print("Fila Vazia")
+    else:
+        key = r.lpop("fila")
+        print(r.hgetall(key))
+        r.delete(key)
     
 while(True):
-    print("\nTRABALHO PRÁTICO - REDIS\n(1) Adicinar uma pessoa na fila\n(2) Exibir fila\n(3) Remover o primeiro da fila")
-    op = int(input("Opção: "))
+    
+    print("\nTRABALHO PRÁTICO - REDIS\n(1) Adicinar uma pessoa na fila\n(2) Exibir fila\n(3) Remover uma pessoa na fila")
+    op = int(input("Opção:"))
 
     if(op == 1):
         Adicionar()
